fa6dd7e3d44645ba33f0e8ef77a0e17d8a1bb59d
Alexander Blunck <alex@blunck.co>
2018-08-16 17:14:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# @blunck/ga
+
+[![Latest Version on NPM](https://img.shields.io/npm/v/@blunck/ga.svg?style=flat-square)](https://www.npmjs.com/package/@blunck/ga)
+
+Simple tool to add Google Analytics tracking code to your website
+
+## Installation
+```bash
+npm install --save-dev @blunck/ja
+```
+
+## Usage
+```javascript
+import ga from '@blunck/js'
+
+// Add gtag.js script & initialize
+ga.add('UA-XXXXXXXXX-X')
+
+// Initialize without anonymizing ip address
+ga.add('UA-XXXXXXXXX-X', add)
+
+// Use window.gtag i.e. to send events
+gtag('event', '<action>', {})
+
+// Use window.gtagConfig to update configuration
+gtagConfig({ 'page_path': '/search' })
+
+// The previous example is short for:
+gtag('config', 'UA-XXXXXXXXX-X', { 'page_path': '/search' })
+```
---
+++ src/index.js
@@ -0,0 +1,29 @@
+export default {
+    add: (trackingId, anonymizeIp = true) => {
+        const elem = document.createElement('script')
+        elem.type = 'text/javascript'
+        elem.async = true
+        elem.src = `https://www.googletagmanager.com/gtag/js?id=${trackingId}`
+
+        // Insert script before first <script> element
+        const script = document.getElementsByTagName('script')[0]
+        script.parentNode.insertBefore(elem, script)
+
+        window.dataLayer = window.dataLayer || []
+
+        window.gtag = function() {
+            window.dataLayer.push(arguments)
+        }
+
+        window.gtagConfig = function(options) {
+            options = Object.assign({}, options, {
+                anonymize_ip: anonymizeIp
+            })
+
+            window.gtag('config', trackingId, options)
+        }
+
+        window.gtag('js', new Date())
+        window.gtagConfig()
+    }
+}
